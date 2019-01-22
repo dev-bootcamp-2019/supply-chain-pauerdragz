4a69cc372cd2e80b6c4dc7f7f26a3258014702cd
pauerdragz <andrei.alexandru.28@gmail.com>
2019-01-22 16:26:15 +0200

    Update SupplyChain.sol

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -133,7 +133,6 @@ contract SupplyChain {
     emit LogShipped(sku);
   }
   
-
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku) shipped(sku) verifyCaller(items[sku].buyer)
